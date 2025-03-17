b9268bfe65fa20c463fa1b92c5979a066e27e9d4
Voltrav2 <enessener634@gmail.com>
2025-03-17 13:40:48 +0300

    Update rp_handler.py

--- rp_handler.py
+++ rp_handler.py
@@ -1,35 +1,18 @@
 import runpod
 import demucs.separate
 import os
-import requests
 
 def handler(event):
-    input_audio = event['input'].get('audio')
+    input_audio = event['input'].get('audio_url')
 
-    if not input_audio:
-        return {"error": "Audio URL is missing."}
+    # Burada Demucs işlemini gerçekleştireceğiz
+    output_folder = "separated"
+    os.system(f"demucs -o {output_folder} {input_audio}")
 
-    # Dosyayı indir
-    local_audio_path = "/tmp/input_audio.mp3"
-    try:
-        response = requests.get(input_audio, stream=True)
-        if response.status_code == 200:
-            with open(local_audio_path, "wb") as audio_file:
-                for chunk in response.iter_content(chunk_size=8192):
-                    audio_file.write(chunk)
-        else:
-            return {"error": "Failed to download audio file."}
-    except Exception as e:
-        return {"error": f"Error downloading file: {str(e)}"}
-
-    # Demucs işlemini çalıştır
-    output_folder = "/app/separated"
-    os.system(f"demucs -o {output_folder} {local_audio_path}")
-
-    # Çıktıları döndür
+    # Ayrıştırılmış dosyaların yollarını döndür
     return {
-        "vocal": f"{output_folder}/htdemucs/input_audio/vocals.wav",
-        "no_vocal": f"{output_folder}/htdemucs/input_audio/no_vocals.wav"
+        "vocal": f"{output_folder}/htdemucs/vocals.wav",
+        "no_vocal": f"{output_folder}/htdemucs/no_vocals.wav"
     }
 
 if __name__ == "__main__":
