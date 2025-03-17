ca96a2a2b82e3233b6c8fe215db663772fc2762e
Voltrav2 <enessener634@gmail.com>
2025-03-17 14:12:10 +0300

    Update rp_handler.py

--- rp_handler.py
+++ rp_handler.py
@@ -1,19 +1,44 @@
 import runpod
-import demucs.separate
+import subprocess
+import shutil
 import os
+import torch
 
 def handler(event):
-    input_audio = event['input'].get('audio_url')
+    """ RunPod Serverless API için Demucs işleyici fonksiyonu """
+    job_id = event['id']
+    audio_url = event['input']['audio_url']
 
-    # Burada Demucs işlemini gerçekleştireceğiz
-    output_folder = "separated"
-    os.system(f"demucs -o {output_folder} {input_audio}")
+    # Müzik dosyasını indir
+    os.system(f"wget -O musics/{job_id}.mp3 {audio_url}")
 
-    # Ayrıştırılmış dosyaların yollarını döndür
+    # Demucs ile ayrıştırma işlemi
+    subprocess.run([
+        "demucs",
+        "--mp3",
+        "--two-stems",
+        "vocals",
+        "-n",
+        "mdx_extra",
+        f"musics/{job_id}.mp3",
+        "-o",
+        "musics"
+    ])
+
+    # Çıktıları taşı
+    output_path = f"musics/{job_id}"
+    os.makedirs(output_path, exist_ok=True)
+    
+    shutil.move(f"musics/mdx_extra/{job_id}/vocals.mp3", f"{output_path}/vocals.mp3")
+    shutil.move(f"musics/mdx_extra/{job_id}/no_vocals.mp3", f"{output_path}/no_vocals.mp3")
+    shutil.rmtree(f"musics/mdx_extra")
+
+    torch.cuda.empty_cache()
+
     return {
-        "vocal": f"{output_folder}/htdemucs/vocals.wav",
-        "no_vocal": f"{output_folder}/htdemucs/no_vocals.wav"
+        "vocal_path": f"{output_path}/vocals.mp3",
+        "no_vocal_path": f"{output_path}/no_vocals.mp3"
     }
 
-if __name__ == "__main__":
-    runpod.serverless.start({"handler": handler})
+if __name__ == '__main__':
+    runpod.serverless.start({'handler': handler})
